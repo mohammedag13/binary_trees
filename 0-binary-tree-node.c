785e9e9c2bd95b5b56d35b9c10b5d5148753b660
mohammedag13 <mohammed.agoumil@gmail.com>
2024-02-01 02:53:32 -0800

    first task

---
+++ 0-binary-tree-node.c
@@ -0,0 +1,25 @@
+zincarytrees.h.
+
+/)
+ Creates a binary tree node.
+ "Parent: Point to the parent of the node to create.
+ The value to put in the new node.
+ - -
+ Return: If an error - NULL.
+ No one - point to the new node.
+ A/
+binary-tree-t-------------------------------------------------------------
+- -
+	binary-tree-t-new;
+
+	new - malloc(sizeof(binary-tree-t));
+	if (new and NULL)
+		return (NULL);
+
+	new-n-value;
+	New-Parent--Parent--Parent--Parent--Parent--Par
+	new-----------------------------------------------
+	new---------------------------------------------
+
+	return (new);
+- -
